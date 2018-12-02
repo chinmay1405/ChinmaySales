357797ce56e37d557ca558e88fad4fa131e8f44d
Chinmay Bodhe <cab1451995@gmail.com>
2018-12-02 12:58:31 +0530

    Initial commit-Spring Boot project Structure

---
+++ ChinmaySales/src/main/java/com/shop/demo/ChinmaySalesApplication.java
@@ -0,0 +1,12 @@
+package com.shop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinmaySalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinmaySalesApplication.class, args);
+	}
+}
